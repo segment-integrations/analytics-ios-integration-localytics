517c48723cf30f15b1c549cb39775ff18f375b5a
Prateek Srivastava <f2prateek@gmail.com>
2016-06-30 14:03:36 -0600

    Update Podfile for CocoaPods 1.0

--- Example/Pods/Target Support Files/Segment-Localytics/Segment-Localytics-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SEGLocalyticsIntegration.h"
-#import "SEGLocalyticsIntegrationFactory.h"
-
-FOUNDATION_EXPORT double Segment_LocalyticsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Segment_LocalyticsVersionString[];
-
